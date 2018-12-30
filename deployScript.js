5dff71f9ffe86faf809cf7358d818c04e318ba65
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-12-30 12:15:21 +0530

    changes in eamil setting

--- deployScript.js
+++ deployScript.js
@@ -1,6 +1,7 @@
 var request = require('request');
-request('http://13.126.135.113:3020/crm', function (error, response, body) {
+request('http://13.232.45.85:3031/crm', function (error, response, body) {
   console.log('error:', error);
   console.log('body:', body);
 });
+
 
--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../environments/environment';
 export const AppSetting: AppSettingType = {
-    awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3020/',
+    awsServiceUrl: 'http://ec2-13-127-165-9.ap-south-1.compute.amazonaws.com:3031/',
     local3021CrmServiceUrl: 'http://localhost:3031/',
     serviceUrl: environment.serviceUrl
     // serviceUrl: 'https://rinteger.com/adminservice/'
